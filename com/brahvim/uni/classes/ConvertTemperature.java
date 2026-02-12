71f772da5ae906452dcf6a817e78640051ca159b
Brahvim <69293652+Brahvim@users.noreply.github.com.>
2026-02-12 14:18:14 +0530

    Weird ones done back when this repo was started.

--- com/brahvim/uni/classes/ConvertTemperature.java
+++ com/brahvim/uni/classes/ConvertTemperature.java
@@ -4,23 +4,23 @@ import java.util.Scanner;
 
 public class ConvertTemperature {
 
-    public static void main(final String... p_args) {
-        try (final Scanner sc = new Scanner((System.in))) {
+	public static void main(final String... p_args) {
+		try (final Scanner sc = new Scanner((System.in))) {
 
-            System.out.print("Enter a temperature in Celsius: ");
-            System.out.printf(
+			System.out.print("Enter a temperature in Celsius: ");
+			System.out.printf(
 
-                    "In Fahrenheit, that is `%.2f`.\n",
-                    (180.0d / 100.0d) * sc.nextDouble() + 32.0d
+					"In Fahrenheit, that is `%.2f`.\n",
+					(180.0d / 100.0d) * sc.nextDouble() + 32.0d
 
-            );
+			);
 
-        } catch (final Exception e) {
+		} catch (final Exception e) {
 
-            System.out.println("There was an error.");
-            // e.printStackTrace();
+			System.out.println("There was an error.");
+			// e.printStackTrace();
 
-        }
-    }
+		}
+	}
 
 }
---
+++ com/brahvim/uni/classes/OperatorHell.java
@@ -0,0 +1,23 @@
+package com.brahvim.uni.classes;
+
+public class OperatorHell {
+
+	public static void main(final String... p_args) {
+		final int x = 3;
+		final int y = 5;
+		final int z = 7;
+		System.out.println(
+
+				(x + y * 2 << 1 & 15) == 10
+						&& !(x > z) || x != y
+
+		);
+		System.out.println(
+
+				(x * y + 10 / 2 % 3 & 7) == 5
+				&& x >= z
+
+		);
+	}
+
+}
